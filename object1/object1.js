dedf9418bbd92123e6cf164dd70b4e13ec389d31
KLiliana <74898690+KLiliana@users.noreply.github.com>
2023-11-23 12:08:33 +0100

    Add files via upload

---
+++ object1/object1.js
@@ -0,0 +1,18 @@
+
+// 1. Hozz létre egy üres user objektet
+let user = {};
+
+// 2. Add a fistName property-t az objektumhoz a John értékkel.
+user.firstName = 'John';
+
+// 3. Add a lastName property-t az objektumhoz a Smith értékkel.
+user.lastName = 'Smith';
+
+// 4. Módosítsd a firstName-et Paul-ra.
+user.firstName = 'Paul';
+
+// 5. Töröld a firstName property-t az objektből.
+delete user.firstName;
+
+// Konzolba írasd ki a user objektet.
+console.log(user);
