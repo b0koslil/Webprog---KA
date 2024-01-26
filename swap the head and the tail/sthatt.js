b551147a2b0f85caa55b57de07bb035e0c6dd6b9
KLiliana <74898690+KLiliana@users.noreply.github.com>
2024-01-26 11:29:11 +0100

    Update sthatt.js

--- swap the head and the tail/sthatt.js
+++ swap the head and the tail/sthatt.js
@@ -5,13 +5,20 @@ function swapHeadTail(arr) {
   
     const head = arr.slice(0, midpoint);
     const tail = arr.slice(midpoint);
+
+     if (arr.length % 2 == 1) {
+      tail.push(head.pop());
+    }
   
-    const result = tail.concat(head.reverse());
-  
+    const result = tail.concat(head);
+
     return result;
   }
   
   console.log(swapHeadTail([1, 2, 3, 4, 5])); 
   console.log(swapHeadTail([-1, 2]));           
   console.log(swapHeadTail([1, 2, -3, 4, 5, 6, -7, 8])); 
+  
+
+
   
