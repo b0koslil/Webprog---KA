682f71de6ae4744c579787e2ac6945cde5e9276e
KLiliana <74898690+KLiliana@users.noreply.github.com>
2024-02-09 09:24:58 +0100

    Add files via upload

---
+++ dolgozat/dolgozat.js
@@ -0,0 +1,68 @@
+// All or Nothing
+function possiblyPerfect(key,answers) {
+
+    let valaszok_ = valaszok.split;
+    let megoldokulcs_ = megoldokulcs.split;
+    let jo = 0;
+    let rossz = 0;
+    let hianyos = 0;
+
+    for (let i = 0, i < megoldokulcs_.length; i++)
+    {
+        if 
+    }
+}
+
+console.log(possiblyPerfect([..."A_C_B"],[..."ADCEB"]) + " >> true ");
+console.log( possiblyPerfect([..."B_B"],[..."BDC"]) + " >> false ");
+console.log( possiblyPerfect([..."T_FFF"],[..."FFTTT"]) + " >> true ");
+console.log( possiblyPerfect([..."BA__"],[..."BACC"]) + " >> true ");
+console.log( possiblyPerfect([..."ABA_"],[..."BACC"]) + " >> true ");
+console.log( possiblyPerfect([..."ABC_"],[..."BACC"]) + " >> false ");
+console.log( possiblyPerfect([..."B_"],[..."CA"]) + " >> true ");
+console.log( possiblyPerfect([..."BA"],[..."CA"]) + " >> false ");
+console.log( possiblyPerfect([..."B"],[..."B"]) + " >> true ");
+console.log( possiblyPerfect([..."_T__"],[..."TFFF"]) + " >> true ");
+console.log( possiblyPerfect([..."_T__"],[..."TTFT"]) + " >> true ");
+console.log( possiblyPerfect([..."_TTT"],[..."TTFT"]) + " >> false ");
+console.log( possiblyPerfect([..."_TTT"],[..."TTTT"]) + " >> true ");
+console.log( possiblyPerfect([..."_TTT"],[..."FFFF"]) + " >> true ");
+console.log( possiblyPerfect([..."____"],[..."FFFF"]) + " >> true ");
+
+
+
+// Mean vs. Median
+function meanVsMedian(numbers) {
+    let mean = ;
+    let median = ;
+    let same = ;
+}
+
+function mean(numbers)
+{
+
+}
+
+function median(numbers)
+{
+
+}
+
+console.log(meanVsMedian([1, 1, 1]), ' >> same');
+console.log(meanVsMedian([1, 2, 37]), ' >> mean');
+console.log(meanVsMedian([7, 14, -70]), ' >> median');
+
+
+
+// Swap the head and the tail
+function swapHeadAndTail(arr) {
+    
+    if (arr.length % 2 == 0)
+    {
+        
+    }
+}
+
+console.log(swapHeadAndTail([ 1, 2, 3, 4, 5 ]), [ 4, 5, 3, 1, 2 ]);
+console.log(swapHeadAndTail([ -1, 2 ]), [ 2, -1 ]);
+console.log(swapHeadAndTail([ 1, 2, -3, 4, 5, 6, -7, 8 ]), [ 5, 6, -7, 8, 1, 2, -3, 4 ]);
